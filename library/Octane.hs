bd706492e725e9e4fb1128988291e9c9ace686e9
Taylor Fausak <taylor@fausak.me>
2015-09-29 15:46:53 -0500

    Parse properties

--- library/Octane.hs
+++ library/Octane.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Octane where
+
+import Control.Monad (replicateM)
 
 import qualified Data.Binary as B
 import qualified Data.Binary.Get as G
+import qualified Data.Binary.IEEE754 as B
 import qualified Data.Binary.Put as P
 import qualified Data.ByteString as BS
+import qualified Data.Map as M
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified System.Environment as E
@@ -26,11 +32,22 @@ printResult result = case result of
 data Replay = NewReplay
     { replayIntro :: BS.ByteString
     , replayLabel :: T.Text
+    , replayProperties :: Properties
     } deriving (Eq, Ord, Read, Show)
 
 instance B.Binary Replay where
     get = getReplay
     put = putReplay
+
+type Properties = M.Map T.Text Property
+
+data Property
+    = ArrayProperty [Properties]
+    | FloatProperty Float
+    | IntProperty Int
+    | NameProperty T.Text
+    | StrProperty T.Text
+    deriving (Eq, Ord, Read, Show)
 
 -- * Readers
 
@@ -42,16 +59,76 @@ getReplay = do
     -- NOTE: This label appears to always be "TAGame.Replay_Soccar_TA".
     label <- getText
 
+    properties <- getProperties
+
     return NewReplay
         { replayIntro = intro
         , replayLabel = label
+        , replayProperties = properties
         }
 
 getText :: G.Get T.Text
 getText = do
     size <- G.getWord32le
     bytes <- G.getByteString (fromIntegral size)
-    return (T.decodeUtf8 bytes)
+    let text = T.dropEnd 1 (T.decodeUtf8 bytes)
+    return text
+
+getProperties :: G.Get Properties
+getProperties = do
+    key <- getText
+    if key == "None"
+    then do
+        return M.empty
+    else do
+        value <- getProperty
+        let property = M.singleton key value
+        properties <- getProperties
+        return (M.union property properties)
+
+getProperty :: G.Get Property
+getProperty = do
+    kind <- getText
+    size <- G.getWord64le
+    property <- case kind of
+        "ArrayProperty" -> getArrayProperty size
+        "FloatProperty" -> getFloatProperty size
+        "IntProperty" -> getIntProperty size
+        "NameProperty" -> getNameProperty size
+        "StrProperty" -> getStrProperty size
+        _ -> fail ("unknown property type " ++ show kind)
+    return property
+
+getArrayProperty :: B.Word64 -> G.Get Property
+getArrayProperty _ = do
+    size <- G.getWord32le
+    array <- replicateM (fromIntegral size) getProperties
+    return (ArrayProperty array)
+
+getFloatProperty :: B.Word64 -> G.Get Property
+getFloatProperty size = do
+    float <- case size of
+        4 -> B.getFloat32le
+        _ -> fail ("unknown FloatProperty size " ++ show size)
+    return (FloatProperty float)
+
+getIntProperty :: B.Word64 -> G.Get Property
+getIntProperty size = do
+    word <- case size of
+        4 -> G.getWord32le
+        _ -> fail ("unknown IntProperty size " ++ show size)
+    let integer = fromIntegral word
+    return (IntProperty integer)
+
+getNameProperty :: B.Word64 -> G.Get Property
+getNameProperty _ = do
+    name <- getText
+    return (NameProperty name)
+
+getStrProperty :: B.Word64 -> G.Get Property
+getStrProperty _ = do
+    string <- getText
+    return (StrProperty string)
 
 -- * Writers
 
