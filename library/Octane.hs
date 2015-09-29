b22cbfd01485faf49b650c2ba9a4a0fabcadc5a7
Taylor Fausak <taylor@fausak.me>
2015-09-29 14:59:39 -0500

    Set up the high-level program structure

--- library/Octane.hs
+++ library/Octane.hs
@@ -1,4 +1,24 @@
 module Octane where
 
+import qualified Data.Binary as B
+import qualified Data.Binary.Get as G
+import qualified System.Environment as E
+
 main :: IO ()
-main = putStrLn "Octane!"
+main = do
+    files <- E.getArgs
+    results <- mapM B.decodeFileOrFail files
+    mapM_ printResult results
+
+printResult :: Either (G.ByteOffset, String) Replay -> IO ()
+printResult result = case result of
+    Left (offset, message) -> putStrLn (show offset ++ ": " ++ message)
+    Right replay -> print replay
+
+data Replay = NewReplay
+    {
+    } deriving (Eq, Ord, Read, Show)
+
+instance B.Binary Replay where
+    get = undefined
+    put = undefined
