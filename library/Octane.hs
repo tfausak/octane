9f4723c3d058f60a4e41abc14782b149adc064f6
Taylor Fausak <taylor@fausak.me>
2015-09-29 15:04:24 -0500

    Create top-level functions for reading and writing replays

--- library/Octane.hs
+++ library/Octane.hs
@@ -2,7 +2,10 @@ module Octane where
 
 import qualified Data.Binary as B
 import qualified Data.Binary.Get as G
+import qualified Data.Binary.Put as P
 import qualified System.Environment as E
+
+-- * High-level interface
 
 main :: IO ()
 main = do
@@ -15,10 +18,28 @@ printResult result = case result of
     Left (offset, message) -> putStrLn (show offset ++ ": " ++ message)
     Right replay -> print replay
 
+-- * Types
+
 data Replay = NewReplay
     {
     } deriving (Eq, Ord, Read, Show)
 
 instance B.Binary Replay where
-    get = undefined
-    put = undefined
+    get = getReplay
+    put = putReplay
+
+-- * Readers
+
+getReplay :: G.Get Replay
+getReplay = do
+    return NewReplay
+        {
+        }
+
+-- * Writers
+
+-- TODO: This library should be able to write replays, but it's not currently a
+--   high priority.
+putReplay :: Replay -> P.Put
+putReplay _replay = do
+    undefined
