ffeb54a2137c0c90a8a1da7b7a84a7670860ad3d
Taylor Fausak <taylor@fausak.me>
2015-09-30 20:33:14 -0500

    Revert "Rewrite replays without outros"
    This reverts commit 3de87476b36c6a6fa0a70e05003cb70897514a6b.

--- library/Octane.hs
+++ library/Octane.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Octane where
 
 import Octane.Types
@@ -7,11 +5,24 @@ import Octane.Types
 import System.Environment (getArgs)
 
 import qualified Data.Binary as B
+import qualified Data.Binary.Get as B
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
 
 main :: IO ()
 main = do
     files <- getArgs
-    replays <- mapM B.decodeFile files
-    let newFiles = map (\ x -> x ++ "-no-outro.replay") files
-    let newReplays = map (\ x -> x { replayOutro = "" }) replays
-    mapM_ (uncurry B.encodeFile) (zip newFiles newReplays)
+    contents <- mapM BS.readFile files
+    results <- mapM B.decodeFileOrFail files
+    mapM_ debug (zip3 files contents results)
+
+debug :: (String, BS.ByteString, Either (B.ByteOffset, String) Replay) -> IO ()
+debug (file, contents, result) = do
+    putStrLn file
+    putStrLn ("input:\t" ++ show (BS.length contents) ++ " bytes")
+    case result of
+        Left (offset, message) -> do
+            putStrLn ("error at byte " ++ show offset ++ ": " ++ message)
+        Right replay -> do
+            let output = B.encode replay
+            putStrLn ("output:\t" ++ show (BSL.length output) ++ " bytes")
