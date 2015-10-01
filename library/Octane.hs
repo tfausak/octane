3de87476b36c6a6fa0a70e05003cb70897514a6b
Taylor Fausak <taylor@fausak.me>
2015-09-30 20:13:14 -0500

    Rewrite replays without outros

--- library/Octane.hs
+++ library/Octane.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Octane where
 
 import Octane.Types
@@ -5,24 +7,11 @@ import Octane.Types
 import System.Environment (getArgs)
 
 import qualified Data.Binary as B
-import qualified Data.Binary.Get as B
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as BSL
 
 main :: IO ()
 main = do
     files <- getArgs
-    contents <- mapM BS.readFile files
-    results <- mapM B.decodeFileOrFail files
-    mapM_ debug (zip3 files contents results)
-
-debug :: (String, BS.ByteString, Either (B.ByteOffset, String) Replay) -> IO ()
-debug (file, contents, result) = do
-    putStrLn file
-    putStrLn ("input:\t" ++ show (BS.length contents) ++ " bytes")
-    case result of
-        Left (offset, message) -> do
-            putStrLn ("error at byte " ++ show offset ++ ": " ++ message)
-        Right replay -> do
-            let output = B.encode replay
-            putStrLn ("output:\t" ++ show (BSL.length output) ++ " bytes")
+    replays <- mapM B.decodeFile files
+    let newFiles = map (\ x -> x ++ "-no-outro.replay") files
+    let newReplays = map (\ x -> x { replayOutro = "" }) replays
+    mapM_ (uncurry B.encodeFile) (zip newFiles newReplays)
