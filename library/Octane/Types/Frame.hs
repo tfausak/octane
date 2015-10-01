8833de292613cd3318c759bf64be45de0d4082b9
Taylor Fausak <taylor@fausak.me>
2015-10-01 08:17:20 -0500

    Reimplement frames as bit-based parsers
    There is no guarantee that they will be byte-aligned. In fact, we
    know they won't be.

--- library/Octane/Types/Frame.hs
+++ library/Octane/Types/Frame.hs
@@ -3,21 +3,26 @@ module Octane.Types.Frame where
 import Octane.Types.Float32LE (Float32LE)
 
 import qualified Data.Binary as B
+import qualified Data.Binary.Bits as Bit
+import qualified Data.Binary.Bits.Get as Bit
+import qualified Data.Binary.Bits.Put as Bit
+import qualified Data.ByteString.Lazy as BSL
 
 data Frame = NewFrame
     { frameTime :: Float32LE
     , frameDelta :: Float32LE
     } deriving (Show)
 
-instance B.Binary Frame where
-    get = do
-        time <- B.get
-        delta <- B.get
+instance Bit.BinaryBit Frame where
+    getBits _ = do
+        timeBytes <- Bit.getByteString 4
+        deltaBytes <- Bit.getByteString 4
         return NewFrame
-            { frameTime = time
-            , frameDelta = delta
+            { frameTime = B.decode (BSL.fromStrict timeBytes)
+            , frameDelta = B.decode (BSL.fromStrict deltaBytes)
             }
 
-    put frame = do
-        B.put (frameTime frame)
-        B.put (frameDelta frame)
+    putBits _ frame = do
+        Bit.joinPut (do
+            B.put (frameTime frame)
+            B.put (frameDelta frame))
