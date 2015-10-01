3dc313dc5a9d9c597a76fb5f17bef7465f0f376b
Taylor Fausak <taylor@fausak.me>
2015-09-30 19:09:08 -0500

    Print different debug information
    Now that I have the whole thing parsed, it's not as useful to dump
    the entire replay out.

--- library/Octane.hs
+++ library/Octane.hs
@@ -1,19 +1,28 @@
 module Octane where
 
-import Octane.Types.Replay (Replay)
+import Octane.Types.Replay
 
 import System.Environment (getArgs)
 
 import qualified Data.Binary as B
 import qualified Data.Binary.Get as B
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
 
 main :: IO ()
 main = do
     files <- getArgs
+    contents <- mapM BS.readFile files
     results <- mapM B.decodeFileOrFail files
-    mapM_ printResult results
+    mapM_ debug (zip3 files contents results)
 
-printResult :: Either (B.ByteOffset, String) Replay -> IO ()
-printResult result = case result of
-    Left (offset, message) -> putStrLn (show offset ++ ": " ++ message)
-    Right replay -> print replay
+debug :: (String, BS.ByteString, Either (B.ByteOffset, String) Replay) -> IO ()
+debug (file, contents, result) = do
+    putStrLn file
+    putStrLn ("input:\t" ++ show (BS.length contents) ++ " bytes")
+    case result of
+        Left (offset, message) -> do
+            putStrLn ("error at byte " ++ show offset ++ ": " ++ message)
+        Right replay -> do
+            let output = B.encode replay
+            putStrLn ("output:\t" ++ show (BSL.length output) ++ " bytes")
