d2167b2d55540f3efc2f3f8a395bba4b9342229d
Taylor Fausak <taylor@fausak.me>
2015-10-06 19:55:27 -0500

    Reformat Message

--- library/Octane/Types/Message.hs
+++ library/Octane/Types/Message.hs
@@ -1,23 +1,28 @@
 module Octane.Types.Message where
 
-import Octane.Types.Int32LE (Int32LE)
-import Octane.Types.PCString (PCString)
+import qualified Data.Binary as Binary
+import Flow ((|>))
+import Octane.Types.Int32LE
+import Octane.Types.PCString
 
-import qualified Data.Binary as B
+data Message = NewMessage {
+    messageFrame :: Int32LE,
+    messageName :: PCString,
+    messageContent :: PCString
+} deriving (Show)
 
-data Message = NewMessage
-    { messageFrame :: Int32LE
-    , messageName :: PCString
-    , messageContent :: PCString
-    } deriving (Show)
-
-instance B.Binary Message where
-    get = NewMessage
-        <$> B.get
-        <*> B.get
-        <*> B.get
+instance Binary.Binary Message where
+    get = do
+        frame <- Binary.get
+        name <- Binary.get
+        content <- Binary.get
+        return NewMessage {
+            messageFrame = frame,
+            messageName = name,
+            messageContent = content
+        }
 
     put message = do
-        B.put (messageFrame message)
-        B.put (messageName message)
-        B.put (messageContent message)
+        message |> messageFrame |> Binary.put
+        message |> messageName |> Binary.put
+        message |> messageContent |> Binary.put
