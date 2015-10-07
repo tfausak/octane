78bc81fc906581e35c1ddfc198d3dceebb195f00
Taylor Fausak <taylor@fausak.me>
2015-10-06 19:48:26 -0500

    Reformat KeyFrame

--- library/Octane/Types/KeyFrame.hs
+++ library/Octane/Types/KeyFrame.hs
@@ -1,23 +1,28 @@
 module Octane.Types.KeyFrame where
 
-import Octane.Types.Float32LE (Float32LE)
-import Octane.Types.Int32LE (Int32LE)
+import qualified Data.Binary as Binary
+import Flow ((|>))
+import Octane.Types.Float32LE
+import Octane.Types.Int32LE
 
-import qualified Data.Binary as B
+data KeyFrame = NewKeyFrame {
+    keyFrameTime :: Float32LE,
+    keyFrameFrame :: Int32LE,
+    keyFramePosition :: Int32LE
+} deriving (Show)
 
-data KeyFrame = NewKeyFrame
-    { keyFrameTime :: Float32LE
-    , keyFrameFrame :: Int32LE
-    , keyFramePosition :: Int32LE
-    } deriving (Show)
-
-instance B.Binary KeyFrame where
-    get = NewKeyFrame
-        <$> B.get
-        <*> B.get
-        <*> B.get
+instance Binary.Binary KeyFrame where
+    get = do
+        time <- Binary.get
+        frame <- Binary.get
+        position <- Binary.get
+        return NewKeyFrame {
+            keyFrameTime = time,
+            keyFrameFrame = frame,
+            keyFramePosition = position
+        }
 
     put keyFrame = do
-        B.put (keyFrameTime keyFrame)
-        B.put (keyFrameFrame keyFrame)
-        B.put (keyFramePosition keyFrame)
+        keyFrame |> keyFrameTime |> Binary.put
+        keyFrame |> keyFrameFrame |> Binary.put
+        keyFrame |> keyFramePosition |> Binary.put
