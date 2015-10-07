39f16c3bd72b8584112d166af37b55a77a707322
Taylor Fausak <taylor@fausak.me>
2015-10-06 19:43:23 -0500

    Reformat Float32LE

--- library/Octane/Types/Float32LE.hs
+++ library/Octane/Types/Float32LE.hs
@@ -1,13 +1,19 @@
 module Octane.Types.Float32LE where
 
-import qualified Data.Binary as B
-import qualified Data.Binary.IEEE754 as B
+import qualified Data.Binary as Binary
+import qualified Data.Binary.IEEE754 as Binary
+import Flow ((|>))
 
-newtype Float32LE = NewFloat32LE
-    { getFloat32LE :: Float
-    } deriving (Show)
+newtype Float32LE = NewFloat32LE {
+    getFloat32LE :: Float
+} deriving (Show)
 
-instance B.Binary Float32LE where
-    get = NewFloat32LE <$> B.getFloat32le
+instance Binary.Binary Float32LE where
+    get = do
+        float32LE <- Binary.getFloat32le
+        return NewFloat32LE {
+            getFloat32LE = float32LE
+        }
 
-    put (NewFloat32LE float) = B.putFloat32le float
+    put (NewFloat32LE float32LE) = do
+        float32LE |> Binary.putFloat32le
