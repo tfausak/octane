506105cb91dad7bc3b7e60e6fe8cf52c89c2a896
Taylor Fausak <taylor@fausak.me>
2015-10-06 20:01:09 -0500

    Reformat PCString

--- library/Octane/Types/PCString.hs
+++ library/Octane/Types/PCString.hs
@@ -2,31 +2,33 @@
 
 module Octane.Types.PCString where
 
-import Octane.Types.Int32LE (Int32LE (NewInt32LE))
+import qualified Data.Binary as Binary
+import qualified Data.Binary.Get as Binary
+import qualified Data.Binary.Put as Binary
+import qualified Data.ByteString as ByteString
+import qualified Data.ByteString.Char8 as ByteString8
+import qualified Data.String as String
+import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text
+import Flow ((|>))
+import Octane.Types.Int32LE
 
-import qualified Data.Binary as B
-import qualified Data.Binary.Get as B
-import qualified Data.Binary.Put as B
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as BS8
-import qualified Data.String as S
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
+newtype PCString = NewPCString {
+    getPCString :: Text.Text
+} deriving (Eq , Ord , String.IsString , Show)
 
-newtype PCString = NewPCString
-    { getPCString :: T.Text
-    } deriving (Eq , Ord , S.IsString , Show)
-
-instance B.Binary PCString where
+instance Binary.Binary PCString where
     get = do
-        (NewInt32LE size) <- B.get
-        bytes <- B.getByteString (fromIntegral size)
-        return (NewPCString (T.dropEnd 1 (T.decodeLatin1 bytes)))
+        (NewInt32LE size) <- Binary.get
+        bytes <- Binary.getByteString size
+        return NewPCString {
+            getPCString = bytes |> Text.decodeLatin1 |> Text.dropEnd 1
+        }
 
     put (NewPCString string) = do
-        let bytes = encodeLatin1 (T.snoc string '\NUL')
-        B.put (NewInt32LE (BS.length bytes))
-        B.putByteString bytes
+        let bytes = string |> flip Text.snoc '\NUL' |> encodeLatin1
+        bytes |> ByteString.length |> NewInt32LE |> Binary.put
+        bytes |> Binary.putByteString
 
-encodeLatin1 :: T.Text -> BS.ByteString
-encodeLatin1 text = BS8.pack (T.unpack text)
+encodeLatin1 :: Text.Text -> ByteString.ByteString
+encodeLatin1 text = text |> Text.unpack |> ByteString8.pack
