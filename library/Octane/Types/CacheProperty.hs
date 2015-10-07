c7c03814a234f652f983c9d5891627119644f7da
Taylor Fausak <taylor@fausak.me>
2015-10-06 19:41:31 -0500

    Reformat CacheProperty

--- library/Octane/Types/CacheProperty.hs
+++ library/Octane/Types/CacheProperty.hs
@@ -1,19 +1,23 @@
 module Octane.Types.CacheProperty where
 
-import Octane.Types.Int32LE (Int32LE)
+import qualified Data.Binary as Binary
+import Flow ((|>))
+import Octane.Types.Int32LE
 
-import qualified Data.Binary as B
+data CacheProperty = NewCacheProperty {
+    cachePropertyIndex :: Int32LE,
+    cachePropertyTag :: Int32LE
+} deriving (Show)
 
-data CacheProperty = NewCacheProperty
-    { cachePropertyIndex :: Int32LE
-    , cachePropertyTag :: Int32LE
-    } deriving (Show)
-
-instance B.Binary CacheProperty where
-    get = NewCacheProperty
-        <$> B.get
-        <*> B.get
+instance Binary.Binary CacheProperty where
+    get = do
+        index <- Binary.get
+        tag <- Binary.get
+        return NewCacheProperty {
+            cachePropertyIndex = index,
+            cachePropertyTag = tag
+        }
 
     put cacheProperty = do
-        B.put (cachePropertyIndex cacheProperty)
-        B.put (cachePropertyTag cacheProperty)
+        cacheProperty |> cachePropertyIndex |> Binary.put
+        cacheProperty |> cachePropertyTag |> Binary.put
