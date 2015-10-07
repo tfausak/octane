b037f74fe4f0e6f580f1a30ee54a004978deaf95
Taylor Fausak <taylor@fausak.me>
2015-10-06 19:37:01 -0500

    Reformat ActorMap

--- library/Octane/Types/ActorMap.hs
+++ library/Octane/Types/ActorMap.hs
@@ -1,21 +1,29 @@
 module Octane.Types.ActorMap where
 
-import Octane.Types.Actor (Actor (NewActor), actorName, actorTag)
-import Octane.Types.Int32LE (Int32LE (NewInt32LE), getInt32LE)
-import Octane.Types.List (List (NewList))
-import Octane.Types.PCString (PCString)
+import qualified Data.Binary as Binary
+import qualified Data.IntMap as IntMap
+import Flow ((|>))
+import Octane.Types.Actor
+import Octane.Types.Int32LE
+import Octane.Types.List
+import Octane.Types.PCString
 
-import qualified Data.Binary as B
-import qualified Data.IntMap as M
+newtype ActorMap = NewActorMap {
+    getActorMap :: IntMap.IntMap PCString
+} deriving (Show)
 
-newtype ActorMap = NewActorMap
-    { getActorMap :: M.IntMap PCString
-    } deriving (Show)
-
-instance B.Binary ActorMap where
+instance Binary.Binary ActorMap where
     get = do
-        NewList list <- B.get
-        return (NewActorMap (M.fromList (map (\ a -> (getInt32LE (actorTag a), actorName a)) list)))
+        actors <- Binary.get
+        return NewActorMap {
+            getActorMap = actors |> getList |> map toTuple |> IntMap.fromList
+        }
 
     put (NewActorMap actorMap) = do
-        B.put (NewList (map (\ (tag, name) -> NewActor { actorName = name, actorTag = NewInt32LE tag }) (M.assocs actorMap)))
+        actorMap |> IntMap.assocs |> map fromTuple |> NewList |> Binary.put
+
+toTuple :: Actor -> (Int, PCString)
+toTuple actor = (actor |> actorTag |> getInt32LE, actor |> actorName)
+
+fromTuple :: (Int, PCString) -> Actor
+fromTuple (tag, name) = NewActor { actorName = name, actorTag = NewInt32LE tag }
