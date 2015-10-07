f51848414ef3e9ccd8bb2fd494a63edb0ea9004d
Taylor Fausak <taylor@fausak.me>
2015-10-06 19:54:11 -0500

    Reformat Mark

--- library/Octane/Types/Mark.hs
+++ library/Octane/Types/Mark.hs
@@ -1,20 +1,24 @@
 module Octane.Types.Mark where
 
-import Octane.Types.Int32LE (Int32LE)
-import Octane.Types.PCString (PCString)
+import qualified Data.Binary as Binary
+import Flow ((|>))
+import Octane.Types.Int32LE
+import Octane.Types.PCString
 
-import qualified Data.Binary as B
+data Mark = NewMark {
+    markLabel :: PCString,
+    markFrame :: Int32LE
+} deriving (Show)
 
-data Mark = NewMark
-    { markLabel :: PCString
-    , markFrame :: Int32LE
-    } deriving (Show)
-
-instance B.Binary Mark where
-    get = NewMark
-        <$> B.get
-        <*> B.get
+instance Binary.Binary Mark where
+    get = do
+        label <- Binary.get
+        frame <- Binary.get
+        return NewMark {
+            markLabel = label,
+            markFrame = frame
+        }
 
     put mark = do
-        B.put (markLabel mark)
-        B.put (markFrame mark)
+        mark |> markLabel |> Binary.put
+        mark |> markFrame |> Binary.put
