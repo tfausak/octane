1f4990a65bcb288027cbd4d6fdc81ce50d3207a2
Taylor Fausak <taylor@fausak.me>
2016-01-31 16:43:36 -0600

    Add benchmarks for Int64LE

---
+++ benchmark/Octane/Type/Primitive/Int64LEBench.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Octane.Type.Primitive.Int64LEBench (benchmarks) where
+
+import Criterion
+import qualified Data.Binary as Binary
+import qualified Data.Binary.Get as Binary
+import qualified Data.ByteString.Lazy as BSL
+import Octane
+
+benchmarks :: Benchmark
+benchmarks = bgroup "Int64LE"
+    [ bgroup "decode"
+        [ bench "basic" (whnf decodeInt64LE "\0\0\0\0")
+        ]
+    , bgroup "encode"
+        [ bench "basic" (whnf Binary.encode (NewInt64LE 0))
+        ]
+    ]
+
+decodeInt64LE :: BSL.ByteString -> Either (BSL.ByteString, Binary.ByteOffset, String) (BSL.ByteString, Binary.ByteOffset, Int64LE)
+decodeInt64LE = Binary.decodeOrFail
--- benchmark/Octane/Type/PrimitiveBench.hs
+++ benchmark/Octane/Type/PrimitiveBench.hs
@@ -4,10 +4,12 @@ import Criterion
 import qualified Octane.Type.Primitive.BooleanBench
 import qualified Octane.Type.Primitive.Float32LEBench
 import qualified Octane.Type.Primitive.Int32LEBench
+import qualified Octane.Type.Primitive.Int64LEBench
 
 benchmarks :: Benchmark
 benchmarks = bgroup "Primitive"
     [ Octane.Type.Primitive.BooleanBench.benchmarks
     , Octane.Type.Primitive.Float32LEBench.benchmarks
     , Octane.Type.Primitive.Int32LEBench.benchmarks
+    , Octane.Type.Primitive.Int64LEBench.benchmarks
     ]
