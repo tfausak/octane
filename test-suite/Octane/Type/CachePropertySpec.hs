0fa59e1e12d793eea6b8dd7e65aac937e5e43b1f
Taylor Fausak <taylor@fausak.me>
2016-02-01 16:32:23 -0600

    Clean up the cache property spec

--- test-suite/Octane/Type/CachePropertySpec.hs
+++ test-suite/Octane/Type/CachePropertySpec.hs
@@ -11,11 +11,39 @@ import Test.Tasty.Hspec
 spec :: Spec
 spec = describe "CacheProperty" $ do
     it "can be decoded" $ do
-        decodeCacheProperty "\0\0\0\0\0\0\0\0" `shouldBe` Right ("", 8, CacheProperty (Int32LE 0) (Int32LE 0))
-        decodeCacheProperty "\1\0\0\0\2\0\0\0" `shouldBe` Right ("", 8, CacheProperty (Int32LE 1) (Int32LE 2))
+        shouldBe
+            (decodeCacheProperty "\
+                \\0\0\0\0\
+                \\0\0\0\0\
+                \")
+            (Right ("", 8, CacheProperty
+                (Int32LE 0)
+                (Int32LE 0)))
+        shouldBe
+            (decodeCacheProperty "\
+                \\1\0\0\0\
+                \\2\0\0\0\
+                \")
+            (Right ("", 8, CacheProperty
+                (Int32LE 1)
+                (Int32LE 2)))
     it "can be encoded" $ do
-        Binary.encode (CacheProperty (Int32LE 0) (Int32LE 0)) `shouldBe` "\0\0\0\0\0\0\0\0"
-        Binary.encode (CacheProperty (Int32LE 1) (Int32LE 2)) `shouldBe` "\1\0\0\0\2\0\0\0"
+        shouldBe
+            (Binary.encode (CacheProperty
+                (Int32LE 0)
+                (Int32LE 0)))
+            "\
+                \\0\0\0\0\
+                \\0\0\0\0\
+                \"
+        shouldBe
+            (Binary.encode (CacheProperty
+                (Int32LE 1) 
+                (Int32LE 2)))
+            "\
+                \\1\0\0\0\
+                \\2\0\0\0\
+                \"
 
 decodeCacheProperty :: BSL.ByteString -> Either (BSL.ByteString, Binary.ByteOffset, String) (BSL.ByteString, Binary.ByteOffset, CacheProperty)
 decodeCacheProperty = Binary.decodeOrFail
