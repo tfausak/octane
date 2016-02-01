239cac458427b1efed387ee747c16956fb14b4d0
Taylor Fausak <taylor@fausak.me>
2016-02-01 16:31:13 -0600

    Clean up the cache item spec

--- test-suite/Octane/Type/CacheItemSpec.hs
+++ test-suite/Octane/Type/CacheItemSpec.hs
@@ -11,11 +11,57 @@ import Test.Tasty.Hspec
 spec :: Spec
 spec = describe "CacheItem" $ do
     it "can be decoded" $ do
-        decodeCacheItem "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0" `shouldBe` Right ("", 16, CacheItem (Int32LE 0) (Int32LE 0) (Int32LE 0) (List []))
-        decodeCacheItem "\1\0\0\0\2\0\0\0\3\0\0\0\1\0\0\0\4\0\0\0\5\0\0\0" `shouldBe` Right ("", 24, CacheItem (Int32LE 1) (Int32LE 2) (Int32LE 3) (List [CacheProperty (Int32LE 4) (Int32LE 5)]))
+        shouldBe
+            (decodeCacheItem "\
+                \\0\0\0\0\
+                \\0\0\0\0\
+                \\0\0\0\0\
+                \\0\0\0\0\
+                \")
+            (Right ("", 16, CacheItem
+                (Int32LE 0)
+                (Int32LE 0)
+                (Int32LE 0)
+                (List [])))
+        shouldBe
+            (decodeCacheItem "\
+                \\1\0\0\0\
+                \\2\0\0\0\
+                \\3\0\0\0\
+                \\1\0\0\0\
+                \\4\0\0\0\5\0\0\0\
+                \")
+            (Right ("", 24, CacheItem
+                (Int32LE 1)
+                (Int32LE 2)
+                (Int32LE 3)
+                (List [CacheProperty (Int32LE 4) (Int32LE 5)])))
     it "can be encoded" $ do
-        Binary.encode (CacheItem (Int32LE 0) (Int32LE 0) (Int32LE 0) (List [])) `shouldBe` "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
-        Binary.encode (CacheItem (Int32LE 1) (Int32LE 2) (Int32LE 3) (List [CacheProperty (Int32LE 4) (Int32LE 5)])) `shouldBe` "\1\0\0\0\2\0\0\0\3\0\0\0\1\0\0\0\4\0\0\0\5\0\0\0"
+        shouldBe
+            (Binary.encode (CacheItem
+                (Int32LE 0)
+                (Int32LE 0)
+                (Int32LE 0)
+                (List [])))
+            "\
+                \\0\0\0\0\
+                \\0\0\0\0\
+                \\0\0\0\0\
+                \\0\0\0\0\
+                \"
+        shouldBe
+            (Binary.encode (CacheItem
+                (Int32LE 1)
+                (Int32LE 2)
+                (Int32LE 3)
+                (List [CacheProperty (Int32LE 4) (Int32LE 5)])))
+            "\
+                \\1\0\0\0\
+                \\2\0\0\0\
+                \\3\0\0\0\
+                \\1\0\0\0\
+                \\4\0\0\0\5\0\0\0\
+                \"
 
 decodeCacheItem :: BSL.ByteString -> Either (BSL.ByteString, Binary.ByteOffset, String) (BSL.ByteString, Binary.ByteOffset, CacheItem)
 decodeCacheItem = Binary.decodeOrFail
