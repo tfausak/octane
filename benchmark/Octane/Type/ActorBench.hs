eef4efc9e754980bbecb83aa131236a7f4507c08
Taylor Fausak <taylor@fausak.me>
2016-01-31 20:46:17 -0600

    Add benchmarks for Actor

--- benchmark/Octane/Type/ActorBench.hs
+++ benchmark/Octane/Type/ActorBench.hs
@@ -1,8 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Octane.Type.ActorBench (benchmarks) where
 
 import Criterion
+import qualified Data.Binary as Binary
+import qualified Data.Binary.Get as Binary
+import qualified Data.ByteString.Lazy as BSL
+import Octane
 
 benchmarks :: Benchmark
 benchmarks = bgroup "Actor"
-    [
+    [ bench "decode basic" (whnf decodeActor "\1\0\0\0\0\0\0\0\0")
+    , bench "encode basic" (whnf Binary.encode (NewActor (NewPCString "") (NewInt32LE 0)))
     ]
+
+decodeActor :: BSL.ByteString -> Either (BSL.ByteString, Binary.ByteOffset, String) (BSL.ByteString, Binary.ByteOffset, Actor)
+decodeActor = Binary.decodeOrFail
