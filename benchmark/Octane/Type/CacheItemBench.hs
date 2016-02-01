2cc29a95ca505298009cbee0525bfb40521eb6c6
Taylor Fausak <taylor@fausak.me>
2016-01-31 20:49:24 -0600

    Add benchmarks for CacheItem

--- benchmark/Octane/Type/CacheItemBench.hs
+++ benchmark/Octane/Type/CacheItemBench.hs
@@ -1,8 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Octane.Type.CacheItemBench (benchmarks) where
 
 import Criterion
+import qualified Data.Binary as Binary
+import qualified Data.Binary.Get as Binary
+import qualified Data.ByteString.Lazy as BSL
+import Octane
 
 benchmarks :: Benchmark
 benchmarks = bgroup "CacheItem"
-    [
+    [ bench "decode basic" (whnf decodeCacheItem "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
+    , bench "encode basic" (whnf Binary.encode (NewCacheItem (NewInt32LE 0) (NewInt32LE 0) (NewInt32LE 0) (NewList [])))
     ]
+
+decodeCacheItem :: BSL.ByteString -> Either (BSL.ByteString, Binary.ByteOffset, String) (BSL.ByteString, Binary.ByteOffset, CacheItem)
+decodeCacheItem = Binary.decodeOrFail
