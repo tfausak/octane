9236cb97b4845151d3f408a4beec245446f27465
Taylor Fausak <taylor@fausak.me>
2016-01-31 20:54:33 -0600

    Add benchmarks for Mark

--- benchmark/Octane/Type/MarkBench.hs
+++ benchmark/Octane/Type/MarkBench.hs
@@ -1,8 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Octane.Type.MarkBench (benchmarks) where
 
 import Criterion
+import qualified Data.Binary as Binary
+import qualified Data.Binary.Get as Binary
+import qualified Data.ByteString.Lazy as BSL
+import Octane
 
 benchmarks :: Benchmark
 benchmarks = bgroup "Mark"
-    [
+    [ bench "decode basic" (whnf decodeMark "\1\0\0\0\0\0\0\0\0")
+    , bench "encode basic" (whnf Binary.encode (NewMark (NewPCString "") (NewInt32LE 0)))
     ]
+
+decodeMark :: BSL.ByteString -> Either (BSL.ByteString, Binary.ByteOffset, String) (BSL.ByteString, Binary.ByteOffset, Mark)
+decodeMark = Binary.decodeOrFail
