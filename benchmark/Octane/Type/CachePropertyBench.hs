239edba93fd8b2475059feff3b67968816824369
Taylor Fausak <taylor@fausak.me>
2016-01-31 20:51:22 -0600

    Add benchmarks for CacheProperty

--- benchmark/Octane/Type/CachePropertyBench.hs
+++ benchmark/Octane/Type/CachePropertyBench.hs
@@ -1,8 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Octane.Type.CachePropertyBench (benchmarks) where
 
 import Criterion
+import qualified Data.Binary as Binary
+import qualified Data.Binary.Get as Binary
+import qualified Data.ByteString.Lazy as BSL
+import Octane
 
 benchmarks :: Benchmark
 benchmarks = bgroup "CacheProperty"
-    [
+    [ bench "decode basic" (whnf decodeCacheProperty "\0\0\0\0\0\0\0\0")
+    , bench "encode basic" (whnf Binary.encode (NewCacheProperty (NewInt32LE 0) (NewInt32LE 0)))
     ]
+
+decodeCacheProperty :: BSL.ByteString -> Either (BSL.ByteString, Binary.ByteOffset, String) (BSL.ByteString, Binary.ByteOffset, CacheProperty)
+decodeCacheProperty = Binary.decodeOrFail
