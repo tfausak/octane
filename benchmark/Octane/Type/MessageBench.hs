6982f57742fb69fa21896fd1d2bd465286d2e501
Taylor Fausak <taylor@fausak.me>
2016-01-31 20:56:08 -0600

    Add benchmarks for Message

--- benchmark/Octane/Type/MessageBench.hs
+++ benchmark/Octane/Type/MessageBench.hs
@@ -1,8 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Octane.Type.MessageBench (benchmarks) where
 
 import Criterion
+import qualified Data.Binary as Binary
+import qualified Data.Binary.Get as Binary
+import qualified Data.ByteString.Lazy as BSL
+import Octane
 
 benchmarks :: Benchmark
 benchmarks = bgroup "Message"
-    [
+    [ bench "decode basic" (whnf decodeMessage "\0\0\0\0\1\0\0\0\0\1\0\0\0\0")
+    , bench "encode basic" (whnf Binary.encode (NewMessage (NewInt32LE 0) (NewPCString "") (NewPCString "")))
     ]
+
+decodeMessage :: BSL.ByteString -> Either (BSL.ByteString, Binary.ByteOffset, String) (BSL.ByteString, Binary.ByteOffset, Message)
+decodeMessage = Binary.decodeOrFail
