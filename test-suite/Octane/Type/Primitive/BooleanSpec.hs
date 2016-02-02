28792c963142c268984e9b092f2dcc4a484ca955
Taylor Fausak <taylor@fausak.me>
2016-02-01 20:23:11 -0600

    Clean up the boolean spec

--- test-suite/Octane/Type/Primitive/BooleanSpec.hs
+++ test-suite/Octane/Type/Primitive/BooleanSpec.hs
@@ -11,13 +11,23 @@ import Test.Tasty.Hspec
 spec :: Spec
 spec = describe "Boolean" $ do
     it "can be decoded" $ do
-        decodeBoolean "\0" `shouldBe` Right ("", 1, Boolean False)
-        decodeBoolean "\1" `shouldBe` Right ("", 1, Boolean True)
+        shouldBe
+            (decodeBoolean "\0")
+            (Right ("", 1, Boolean False))
+        shouldBe
+            (decodeBoolean "\1")
+            (Right ("", 1, Boolean True))
     it "can be encoded" $ do
-        Binary.encode (Boolean False) `shouldBe` "\0"
-        Binary.encode (Boolean True) `shouldBe` "\1"
+        shouldBe
+            (Binary.encode (Boolean False))
+            "\0"
+        shouldBe
+            (Binary.encode (Boolean True))
+            "\1"
     it "does not raise a runtime error when decoding garbage" $ do
-        decodeBoolean "garbage" `shouldBe` Left ("arbage", 1, "out of bounds")
+        shouldBe
+            (decodeBoolean "garbage")
+            (Left ("arbage", 1, "out of bounds"))
 
 decodeBoolean :: BSL.ByteString -> Either (BSL.ByteString, Binary.ByteOffset, String) (BSL.ByteString, Binary.ByteOffset, Boolean)
 decodeBoolean = Binary.decodeOrFail
