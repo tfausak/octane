51dba89e33c065ab99e49fa1e80b81362b1df23e
Taylor Fausak <taylor@fausak.me>
2016-02-01 19:57:48 -0600

    Clean up the message spec

--- test-suite/Octane/Type/MessageSpec.hs
+++ test-suite/Octane/Type/MessageSpec.hs
@@ -11,11 +11,41 @@ import Test.Tasty.Hspec
 spec :: Spec
 spec = describe "Message" $ do
     it "can be decoded" $ do
-        decodeMessage "\0\0\0\0\1\0\0\0\0\1\0\0\0\0" `shouldBe` Right ("", 14, Message (Int32LE 0) (PCString "") (PCString ""))
-        decodeMessage "\1\0\0\0\2\0\0\0a\0\2\0\0\0b\0" `shouldBe` Right ("", 16, Message (Int32LE 1) (PCString "a") (PCString "b"))
+        shouldBe
+            (decodeMessage "\
+                \\0\0\0\0\
+                \\1\0\0\0\0\
+                \\1\0\0\0\0\
+                \")
+            (Right ("", 14, Message
+                (Int32LE 0)
+                (PCString "")
+                (PCString "")))
+        shouldBe
+            (decodeMessage "\
+                \\1\0\0\0\
+                \\2\0\0\0a\0\
+                \\2\0\0\0b\0\
+                \")
+            (Right ("", 16, Message
+                (Int32LE 1)
+                (PCString "a")
+                (PCString "b")))
     it "can be encoded" $ do
-        Binary.encode (Message (Int32LE 0) (PCString "") (PCString "")) `shouldBe` "\0\0\0\0\1\0\0\0\0\1\0\0\0\0"
-        Binary.encode (Message (Int32LE 1) (PCString "a") (PCString "b")) `shouldBe` "\1\0\0\0\2\0\0\0a\0\2\0\0\0b\0"
+        shouldBe
+            (Binary.encode (Message (Int32LE 0) (PCString "") (PCString "")))
+            "\
+                \\0\0\0\0\
+                \\1\0\0\0\0\
+                \\1\0\0\0\0\
+                \"
+        shouldBe
+            (Binary.encode (Message (Int32LE 1) (PCString "a") (PCString "b")))
+            "\
+                \\1\0\0\0\
+                \\2\0\0\0a\0\
+                \\2\0\0\0b\0\
+                \"
 
 decodeMessage :: BSL.ByteString -> Either (BSL.ByteString, Binary.ByteOffset, String) (BSL.ByteString, Binary.ByteOffset, Message)
 decodeMessage = Binary.decodeOrFail
