0b9a8488793b85ff3897076c6bd9a764a21f968d
Taylor Fausak <taylor@fausak.me>
2016-02-01 20:22:13 -0600

    Clean up the float spec

--- test-suite/Octane/Type/Primitive/Float32LESpec.hs
+++ test-suite/Octane/Type/Primitive/Float32LESpec.hs
@@ -11,11 +11,19 @@ import Test.Tasty.Hspec
 spec :: Spec
 spec = describe "Float32LE" $ do
     it "can be decoded" $ do
-        decodeFloat32LE "\0\0\0\0" `shouldBe` Right ("", 4, Float32LE 0.0)
-        decodeFloat32LE "\0\0\128\63" `shouldBe` Right ("", 4, Float32LE 1.0)
+        shouldBe
+            (decodeFloat32LE "\0\0\0\0")
+            (Right ("", 4, Float32LE 0.0))
+        shouldBe
+            (decodeFloat32LE "\0\0\128\63")
+            (Right ("", 4, Float32LE 1.0))
     it "can be encoded" $ do
-        Binary.encode (Float32LE 0.0) `shouldBe` "\0\0\0\0"
-        Binary.encode (Float32LE 1.0) `shouldBe` "\0\0\128\63"
+        shouldBe
+            (Binary.encode (Float32LE 0.0))
+            "\0\0\0\0"
+        shouldBe
+            (Binary.encode (Float32LE 1.0))
+            "\0\0\128\63"
 
 decodeFloat32LE :: BSL.ByteString -> Either (BSL.ByteString, Binary.ByteOffset, String) (BSL.ByteString, Binary.ByteOffset, Float32LE)
 decodeFloat32LE = Binary.decodeOrFail
