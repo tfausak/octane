5f662ec3967bc7f8af91ec95499ac11cca97d490
Taylor Fausak <taylor@fausak.me>
2016-02-05 07:57:34 -0600

    Revert "Remove binary-bits"
    This reverts commit 751b80ca4b8614631bc687e9b616a8f5182250fd.

--- library/Octane/Type/Frame.hs
+++ library/Octane/Type/Frame.hs
@@ -3,6 +3,10 @@
 
 module Octane.Type.Frame (Frame(..)) where
 
+import qualified Data.Binary.Bits as Bits
+import qualified Data.Binary.Bits.Get as Bits
+import qualified Data.Binary.Bits.Put as Bits
+import qualified Data.Word as Word
 import Octane.Core
 import Octane.Type.Primitive.Float32LE
 
@@ -23,3 +27,21 @@ instance Binary Frame where
     put frame = do
         frame & frameTime & put
         frame & frameDelta & put
+
+instance Bits.BinaryBit Frame where
+    getBits _ = do
+        time <- Bits.getWord32be 32
+        delta <- Bits.getWord32be 32
+        return Frame
+            { frameTime = wordToFloat time
+            , frameDelta = wordToFloat delta
+            }
+    putBits _ frame = do
+        frame & frameTime & floatToWord & Bits.putWord32be 32
+        frame & frameDelta & floatToWord & Bits.putWord32be 32
+
+wordToFloat :: Word.Word32 -> Float32LE
+wordToFloat _ = Float32LE 0 -- TODO
+
+floatToWord :: Float32LE -> Word.Word32
+floatToWord _ = 0 -- TODO
