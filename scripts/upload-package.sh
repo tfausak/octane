660497fc83255f3f90fe63e1ba96ab2bcf661c5b
Taylor Fausak <taylor@fausak.me>
2016-03-22 09:58:57 -0500

    Push to Hackage use Travis's deploy provider

--- scripts/upload-package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -o errexit -o verbose
-
-test "$TRAVIS_TAG" &&
-  mkdir -p "$HOME/.stack/upload" &&
-  echo "{ \"username\": \"$HACKAGE_USERNAME\", \"password\": \"$HACKAGE_PASSWORD\" }" > "$HOME/.stack/upload/credentials.json" &&
-  stack upload .
