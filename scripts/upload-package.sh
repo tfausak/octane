f87c7b5376be01a19bdc2fdb948672c1519c3c69
Taylor Fausak <taylor@fausak.me>
2016-03-25 07:54:02 -0500

    Inline scripts

--- scripts/upload-package.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -o errexit -o verbose
-
-test "$TRAVIS_TAG"
-test "$TRAVIS_OS_NAME" = 'linux'
-mkdir -p "$HOME/.stack/upload"
-echo "{ \"username\": \"$HACKAGE_USERNAME\", \"password\": \"$HACKAGE_PASSWORD\" }" > "$HOME/.stack/upload/credentials.json"
-stack upload .
