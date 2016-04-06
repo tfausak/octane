db52433819ef5c0b474da391dcbee9f8c7792f74
Taylor Fausak <taylor@fausak.me>
2016-04-06 09:11:35 -0500

    Qualify all imports and reformat

--- library/Octane/Main.hs
+++ library/Octane/Main.hs
@@ -1,9 +1,5 @@
 module Octane.Main (main) where
 
-import Octane.Internal.Core
-import Octane.Parser
-import Octane.Type
-
 import qualified Control.Monad as Monad
 import qualified Data.Aeson.Encode.Pretty as Aeson
 import qualified Data.Binary as Binary
@@ -11,6 +7,9 @@ import qualified Data.Binary.Get as Binary
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString.Lazy.Char8 as BSL8
+import Data.Function ((&))
+import qualified Octane.Parser as Parser
+import qualified Octane.Type as Type
 import qualified System.Environment as Environment
 import qualified System.IO as IO
 
@@ -21,18 +20,29 @@ main = do
     results <- mapM Binary.decodeFileOrFail files
     mapM_ debug (zip3 files contents results)
 
-debug :: (String, ByteString, Either (Binary.ByteOffset, String) Replay) -> IO ()
-debug (file, contents, result) = case result of
-    Left (offset, message) -> IO.hPutStrLn IO.stderr
-        (file ++ " @ byte " ++ show offset ++ " - " ++ message)
-    Right replay -> do
-        let inputSize = contents & BS.length & fromIntegral
-        let outputSize = replay & Binary.encode & BSL.length
-        Monad.when (inputSize /= outputSize) $ do
-            IO.hPutStrLn IO.stderr ("input size (" ++ show inputSize ++ ") not equal to output size (" ++ show outputSize ++ ")!")
-        let config = Aeson.defConfig { Aeson.confCompare = compare }
-        replay & Aeson.encodePretty' config & BSL8.putStrLn
-
-        let frames = parseFrames replay
-        let frame = take 1 frames
-        print frame
+debug :: (String, BS.ByteString, Either (Binary.ByteOffset, String) Type.Replay)
+      -> IO ()
+debug (file,contents,result) =
+    case result of
+        Left (offset,message) ->
+            IO.hPutStrLn
+                IO.stderr
+                (file ++ " @ byte " ++ show offset ++ " - " ++ message)
+        Right replay -> do
+            let inputSize = contents & BS.length & fromIntegral
+            let outputSize = replay & Binary.encode & BSL.length
+            Monad.when (inputSize /= outputSize) $
+                do IO.hPutStrLn
+                       IO.stderr
+                       ("input size (" ++
+                        show inputSize ++
+                        ") not equal to output size (" ++
+                        show outputSize ++ ")!")
+            let config =
+                    Aeson.defConfig
+                    { Aeson.confCompare = compare
+                    }
+            replay & Aeson.encodePretty' config & BSL8.putStrLn
+            let frames = Parser.parseFrames replay
+            let frame = take 1 frames
+            print frame
--- library/Octane/Parser.hs
+++ library/Octane/Parser.hs
@@ -1,30 +1,32 @@
 module Octane.Parser where
 
-import Octane.Internal.Core
-import Octane.Type
-
+import qualified Control.Newtype as Newtype
 import qualified Data.Binary.Bits.Get as Bits
 import qualified Data.Binary.Get as Binary
 import qualified Data.Bits as Bits
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
+import Data.Function ((&))
+import qualified Octane.Type as Type
 
-parseFrames :: Replay -> [Frame]
-parseFrames replay = Binary.runGet
-    (replay & extractContext & getFrames & Bits.runBitGet)
-    (replay & replayStream & unpack & BSL.fromStrict)
+parseFrames :: Type.Replay -> [Type.Frame]
+parseFrames replay =
+    Binary.runGet
+        (replay & extractContext & getFrames & Bits.runBitGet)
+        (replay & Type.replayStream & Newtype.unpack & BSL.fromStrict)
 
 -- TODO: This will need at least the actors and cache items.
 data Context = Context
     {
     }
 
-extractContext :: Replay -> Context
-extractContext _replay = Context
+extractContext :: Type.Replay -> Context
+extractContext _replay =
+    Context
     {
     }
 
-getFrames :: Context -> Bits.BitGet [Frame]
+getFrames :: Context -> Bits.BitGet [Type.Frame]
 getFrames context = do
     maybeFrame <- getFrame context
     case maybeFrame of
@@ -33,74 +35,85 @@ getFrames context = do
             frames <- getFrames context
             return (frame : frames)
 
-getFrame :: Context -> Bits.BitGet (Maybe Frame)
+getFrame :: Context -> Bits.BitGet (Maybe Type.Frame)
 getFrame context = do
     -- TODO: Convert time bytes into a float.
     time <- Bits.getByteString 32
     -- TODO: Convert delta bytes into a float.
     delta <- Bits.getByteString 32
-
     if BS.all (== 0) time && BS.all (== 0) delta
-    then return Nothing
-    else do
-        replications <- getReplications context
+        then return Nothing
+        else do
+            replications <- getReplications context
+            let frame =
+                    Type.Frame
+                    { Type.frameTime = time
+                    , Type.frameDelta = delta
+                    , Type.frameReplications = replications
+                    }
+            return (Just frame)
 
-        let frame = Frame
-                { frameTime = time
-                , frameDelta = delta
-                , frameReplications = replications
-                }
-
-        return (Just frame)
-
-getReplications :: Context -> Bits.BitGet [Replication]
+getReplications :: Context -> Bits.BitGet [Type.Replication]
 getReplications context = do
-    (context', maybeReplication) <- getReplication context
+    (context',maybeReplication) <- getReplication context
     case maybeReplication of
         Nothing -> return []
         Just replication -> do
             replications <- getReplications context'
             return (replication : replications)
 
-getReplication :: Context -> Bits.BitGet (Context, Maybe Replication)
+getReplication :: Context -> Bits.BitGet (Context, Maybe Type.Replication)
 getReplication context = do
     hasReplication <- Bits.getBool
     if not hasReplication
-    then return (context, Nothing)
-    else do
-        -- TODO: Convert actor ID into an integer.
-        actorId <- Bits.getByteString (bitSize maxChannels)
-        isOpen <- Bits.getBool
-        if isOpen
-        then do
-            isNew <- Bits.getBool
-            if isNew
-            then do
-                _unknownFlag <- Bits.getBool
-                _objectId <- getInt 32
-                -- TODO: Parse new actor.
-                return (context, Just (Replication
-                    { replicationActorId = actorId
-                    , replicationIsOpen = isOpen
-                    , replicationIsNew = Just isNew
-                    }))
-            else do
-                -- TODO: Parse existing actor.
-                return (context, Just (Replication
-                    { replicationActorId = actorId
-                    , replicationIsOpen = isOpen
-                    , replicationIsNew = Just isNew
-                    }))
-        else return (context, Just (Replication
-            { replicationActorId = actorId
-            , replicationIsOpen = isOpen
-            , replicationIsNew = Nothing
-            }))
+        then return (context, Nothing)
+        else do
+            -- TODO: Convert actor ID into an integer.
+            actorId <- Bits.getByteString (bitSize maxChannels)
+            isOpen <- Bits.getBool
+            if isOpen
+                then do
+                    isNew <- Bits.getBool
+                    if isNew
+                        then do
+                            _unknownFlag <- Bits.getBool
+                            _objectId <- getInt 32
+                            -- TODO: Parse new actor.
+                            return
+                                ( context
+                                , Just
+                                      (Type.Replication
+                                       { Type.replicationActorId = actorId
+                                       , Type.replicationIsOpen = isOpen
+                                       , Type.replicationIsNew = Just isNew
+                                       }))
+                        else do
+                            -- TODO: Parse existing actor.
+                            return
+                                ( context
+                                , Just
+                                      (Type.Replication
+                                       { Type.replicationActorId = actorId
+                                       , Type.replicationIsOpen = isOpen
+                                       , Type.replicationIsNew = Just isNew
+                                       }))
+                else return
+                         ( context
+                         , Just
+                               (Type.Replication
+                                { Type.replicationActorId = actorId
+                                , Type.replicationIsOpen = isOpen
+                                , Type.replicationIsNew = Nothing
+                                }))
 
-maxChannels :: (Integral a) => a
+maxChannels
+    :: (Integral a)
+    => a
 maxChannels = 1024
 
-bitSize :: (Integral a) => a -> a
+bitSize
+    :: (Integral a)
+    => a -> a
 bitSize x = x & fromIntegral & logBase (2 :: Double) & ceiling
 
 -- Reads an integer bitwise. The bits of the integer are backwards, so the
@@ -112,15 +125,19 @@ bitSize x = x & fromIntegral & logBase (2 :: Double) & ceiling
 -- For example, if the maximum value is 4 and "11" has been read already,
 -- nothing more will be read because another "1" would put the value over the
 -- maximum.
-getInt :: Int -> Bits.BitGet Int
+getInt
+    :: Int -> Bits.BitGet Int
 getInt maxValue = do
     let maxBits = bitSize maxValue
         go i value = do
             let x = Bits.shiftL 1 i
             if i < maxBits && value + x <= maxValue
-            then do
-                bit <- Bits.getBool
-                let newValue = if bit then value + x else value
-                go (i + 1) newValue
-            else return value
+                then do
+                    bit <- Bits.getBool
+                    let newValue =
+                            if bit
+                                then value + x
+                                else value
+                    go (i + 1) newValue
+                else return value
     go 0 0
