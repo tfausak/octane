332bfc714e0caa0b88fc507d58185677f1e62c05
Taylor Fausak <taylor@fausak.me>
2016-05-31 15:36:46 -0500

    Try to fix caching on AppVeyor

--- tools/install-github-release.ps1
+++ tools/install-github-release.ps1
@@ -1,11 +1,11 @@
-if (Test-Path "$env:APPDATA/local/bin/github-release.exe") {
+if (Test-Path C:\bin\github-release.exe) {
   echo "GitHub Release is already installed."
 } else {
   echo "Installing GitHub Release for windows..."
   curl -OutFile github-release.zip -Uri "https://github.com/tfausak/github-release/releases/download/0.1.9/github-release-0.1.9-windows.zip"
   7z x github-release.zip github-release.exe
-  mkdir "$env:APPDATA/local/bin"
-  mv github-release.exe "$env:APPDATA/local/bin"
+  mkdir C:\bin
+  mv github-release.exe C:\bin
 }
 
 github-release.exe version
--- tools/install-stack.ps1
+++ tools/install-stack.ps1
@@ -1,11 +1,11 @@
-if (Test-Path "$env:APPDATA/local/bin/stack.exe") {
+if (Test-Path C:\bin\stack.exe) {
   echo "Stack is already installed."
 } else {
   echo "Installing Stack for windows..."
   curl -OutFile stack.zip -Uri https://www.stackage.org/stack/windows-x86_64
   7z x stack.zip stack.exe
-  mkdir "$env:APPDATA/local/bin"
-  mv stack.exe "$env:APPDATA/local/bin"
+  mkdir C:\bin
+  mv stack.exe C:\bin
 }
 
 stack.exe --version
