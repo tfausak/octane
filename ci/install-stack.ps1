9e52de49cfa013a8ed3c027eca57e6d3824b6e9d
Taylor Fausak <taylor@fausak.me>
2016-05-31 08:26:52 -0500

    Try to fix Windows build

--- ci/install-stack.ps1
+++ ci/install-stack.ps1
@@ -2,7 +2,7 @@ if (Test-Path "$env:APPDATA/local/bin/stack.exe") {
   echo "Stack is already installed."
 } else {
   echo "Installing Stack for windows..."
-  curl --output stack.zip --location https://www.stackage.org/stack/windows-x86_64
+  curl -OutFile stack.zip -Uri https://www.stackage.org/stack/windows-x86_64
   7z x stack.zip stack.exe
   mv stack.exe "$env:APPDATA/local/bin"
 }
