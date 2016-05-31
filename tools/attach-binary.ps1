56879c7ba55b01297e5e80d4bf72cff050c6d484
Taylor Fausak <taylor@fausak.me>
2016-05-31 11:42:35 -0500

    Fix typo

--- tools/attach-binary.ps1
+++ tools/attach-binary.ps1
@@ -1,12 +1,12 @@
 if ($env:APPVEYOR_REPO_TAG_NAME) {
-  if ($end:GITHUB_TOKEN) {
+  if ($env:GITHUB_TOKEN) {
     echo "Attaching binary for windows to $env:APPVEYOR_REPO_TAG_NAME..."
     7z a octane.zip "$(./stack.exe path --local-install-root)/bin/octane"
     github-release.exe upload --token "$env:GITHUB_TOKEN" --owner tfausak --repo octane --tag "$env:APPVEYOR_REPO_TAG_NAME" --file octane.zip --name "octane-$env:APPVEYOR_REPO_TAG_NAME-windows.zip"
-  } Else {
+  } else {
     echo "The GITHUB_TOKEN environment variable is not set!"
     exit 1
   }
-} Else {
+} else {
   echo "This is not a release build."
 }
