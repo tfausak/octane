a8bce224a9988e01ea2373ceecc92d8e5bab8e0f
Taylor Fausak <taylor@fausak.me>
2016-05-31 15:06:58 -0500

    The Stack executable is on the path now

--- tools/attach-binary.ps1
+++ tools/attach-binary.ps1
@@ -1,7 +1,7 @@
 if ($env:APPVEYOR_REPO_TAG_NAME) {
   if ($env:GITHUB_TOKEN) {
     echo "Attaching binary for windows to $env:APPVEYOR_REPO_TAG_NAME..."
-    7z a octane.zip "$(./stack.exe path --local-install-root)/bin/octane"
+    7z a octane.zip "$(stack.exe path --local-install-root)/bin/octane"
     github-release.exe upload --token "$env:GITHUB_TOKEN" --owner tfausak --repo octane --tag "$env:APPVEYOR_REPO_TAG_NAME" --file octane.zip --name "octane-$env:APPVEYOR_REPO_TAG_NAME-windows.zip"
   } else {
     echo "The GITHUB_TOKEN environment variable is not set!"
