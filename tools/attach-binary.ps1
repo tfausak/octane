9b0396a66d9dec7822589f16a6123a48ce5802c4
Taylor Fausak <taylor@fausak.me>
2016-06-01 08:09:13 -0500

    Fix path to executable on Windows
    For some asinine reason it uses "/c/", which isn't a real path, instead
    of "C:/".

--- tools/attach-binary.ps1
+++ tools/attach-binary.ps1
@@ -1,7 +1,9 @@
 if ($env:APPVEYOR_REPO_TAG_NAME) {
   if ($env:GITHUB_TOKEN) {
     echo "Attaching binary for windows to $env:APPVEYOR_REPO_TAG_NAME..."
-    7z a octane.zip "$(stack.exe exec which octane)"
+    $path = $(stack.exe exec which octane)
+    $path = $path.replace('/c/', 'C:/')
+    7z a octane.zip "$path"
     github-release.exe upload --token "$env:GITHUB_TOKEN" --owner tfausak --repo octane --tag "$env:APPVEYOR_REPO_TAG_NAME" --file octane.zip --name "octane-$env:APPVEYOR_REPO_TAG_NAME-windows.zip"
   } else {
     echo "The GITHUB_TOKEN environment variable is not set!"
