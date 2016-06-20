b5e347fe167b289e37487f59bcba6c1bfa2d0c5b
Taylor Fausak <taylor@fausak.me>
2016-06-19 19:53:42 -0500

    Stub out some of the generator

--- library/Octane/Utility/Generator.hs
+++ library/Octane/Utility/Generator.hs
@@ -1,10 +1,16 @@
 module Octane.Utility.Generator (generateStream) where
 
-import qualified Data.ByteString.Lazy as LazyBytes
+import Data.Function ((&))
+
+import qualified Data.Binary.Bits as BinaryBit
+import qualified Data.Binary.Bits.Put as BinaryBit
+import qualified Data.Binary.Put as Binary
+import qualified Octane.Type.Boolean as Boolean
 import qualified Octane.Type.CacheItem as CacheItem
 import qualified Octane.Type.ClassItem as ClassItem
 import qualified Octane.Type.Frame as Frame
 import qualified Octane.Type.List as List
+import qualified Octane.Type.Replication as Replication
 import qualified Octane.Type.Stream as Stream
 import qualified Octane.Type.Text as Text
 
@@ -17,5 +23,58 @@ generateStream
     -> List.List ClassItem.ClassItem
     -> List.List CacheItem.CacheItem
     -> Stream.Stream
-generateStream _frames _objects _names _classes _cache = do
-    Stream.Stream LazyBytes.empty -- TODO
+generateStream frames _objects _names _classes _cache = do
+    let bitPut = putFrames frames
+    let bytePut = BinaryBit.runBitPut bitPut
+    let bytes = Binary.runPut bytePut
+    Stream.Stream bytes
+
+
+putFrames :: [Frame.Frame] -> BinaryBit.BitPut ()
+putFrames frames = do
+    case frames of
+        [] -> pure ()
+        frame : rest -> do
+            putFrame frame
+            putFrames rest
+
+
+putFrame :: Frame.Frame -> BinaryBit.BitPut ()
+putFrame frame = do
+    frame & Frame.time & BinaryBit.putBits 32
+    frame & Frame.delta & BinaryBit.putBits 32
+    frame & Frame.replications & putReplications
+
+
+putReplications :: [Replication.Replication] -> BinaryBit.BitPut ()
+putReplications replications = do
+    case replications of
+        [] -> do
+            False & Boolean.Boolean & BinaryBit.putBits 1
+        replication : rest -> do
+            True & Boolean.Boolean & BinaryBit.putBits 1
+            putReplication replication
+            putReplications rest
+
+
+putReplication :: Replication.Replication -> BinaryBit.BitPut ()
+putReplication replication = do
+    replication & Replication.actorId & putActorId
+    case Replication.state replication of
+        _ -> pure ()
+
+
+--
+
+
+putActorId :: Word -> BinaryBit.BitPut ()
+putActorId actorId = putCompressedWord maxActorId actorId
+
+
+maxActorId :: Word
+maxActorId = 1024
+
+
+putCompressedWord :: Word -> Word -> BinaryBit.BitPut ()
+putCompressedWord _maxValue _value = do
+    pure () -- TODO
