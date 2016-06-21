7aa5371da1e87c105b0c1f120cab9a2d7ca4a733
Taylor Fausak <taylor@fausak.me>
2016-06-21 17:28:20 -0500

    Store properties as JSON

--- library/Octane/Data/Properties.hs
+++ library/Octane/Data/Properties.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Octane.Data.Properties
     ( booleanProperties
     , byteProperties
@@ -26,269 +29,202 @@ module Octane.Data.Properties
 
 import Data.Function ((&))
 
+import qualified Data.Aeson as Aeson
+import qualified Data.FileEmbed as FileEmbed
+import qualified Data.Map.Strict as Map
+import qualified Data.Maybe as Maybe
 import qualified Data.Set as Set
 import qualified Data.Text as StrictText
 
+-- $
+-- >>> :set -XOverloadedStrings
+
 
 -- | A set of properties that are booleans.
+--
+-- >>> Set.member "Engine.Actor:bBlockActors" booleanProperties
+-- True
 booleanProperties :: Set.Set StrictText.Text
-booleanProperties =
-    [ "Engine.Actor:bBlockActors"
-    , "Engine.Actor:bCollideActors"
-    , "Engine.Actor:bCollideWorld"
-    , "Engine.Actor:bHardAttach"
-    , "Engine.Actor:bHidden"
-    , "Engine.Actor:bNetOwner"
-    , "Engine.Actor:bProjTarget"
-    , "Engine.Actor:bTearOff"
-    , "Engine.GameReplicationInfo:bMatchIsOver"
-    , "Engine.Pawn:bCanSwatTurn"
-    , "Engine.Pawn:bRootMotionFromInterpCurve"
-    , "Engine.Pawn:bSimulateGravity"
-    , "Engine.PlayerReplicationInfo:bAdmin"
-    , "Engine.PlayerReplicationInfo:bBot"
-    , "Engine.PlayerReplicationInfo:bFromPreviousLevel"
-    , "Engine.PlayerReplicationInfo:bIsSpectator"
-    , "Engine.PlayerReplicationInfo:bOnlySpectator"
-    , "Engine.PlayerReplicationInfo:bOutOfLives"
-    , "Engine.PlayerReplicationInfo:bReadyToPlay"
-    , "Engine.PlayerReplicationInfo:bWaitingPlayer"
-    , "ProjectX.GRI_X:bGameStarted"
-    , "TAGame.CameraSettingsActor_TA:bUsingBehindView"
-    , "TAGame.CameraSettingsActor_TA:bUsingSecondaryCamera"
-    , "TAGame.CarComponent_Boost_TA:bNoBoost"
-    , "TAGame.CarComponent_Boost_TA:bUnlimitedBoost"
-    , "TAGame.CarComponent_FlipCar_TA:bFlipRight"
-    , "TAGame.GameEvent_Soccar_TA:bBallHasBeenHit"
-    , "TAGame.GameEvent_Soccar_TA:bOverTime"
-    , "TAGame.GameEvent_TA:bHasLeaveMatchPenalty"
-    , "TAGame.GameEvent_Team_TA:bDisableMutingOtherTeam"
-    , "TAGame.PRI_TA:bIsInSplitScreen"
-    , "TAGame.PRI_TA:bMatchMVP"
-    , "TAGame.PRI_TA:bOnlineLoadoutSet"
-    , "TAGame.PRI_TA:bReady"
-    , "TAGame.PRI_TA:bUsingBehindView"
-    , "TAGame.PRI_TA:bUsingFreecam"
-    , "TAGame.PRI_TA:bUsingSecondaryCamera"
-    , "TAGame.PRI_TA:bVoteToForfeitDisabled"
-    , "TAGame.RBActor_TA:bFrozen"
-    , "TAGame.RBActor_TA:bReplayActor"
-    , "TAGame.Vehicle_TA:bDriving"
-    , "TAGame.Vehicle_TA:bReplicatedHandbrake"
-    ] & map StrictText.pack & Set.fromList
+booleanProperties = Map.findWithDefault Set.empty "boolean" properties
 
 
 -- | A set of properties that are bytes.
+--
+-- >>> Set.member "Engine.PlayerReplicationInfo:Ping" byteProperties
+-- True
 byteProperties :: Set.Set StrictText.Text
-byteProperties =
-    [ "Engine.PlayerReplicationInfo:Ping"
-    , "TAGame.Ball_TA:HitTeamNum"
-    , "TAGame.CameraSettingsActor_TA:CameraPitch"
-    , "TAGame.CameraSettingsActor_TA:CameraYaw"
-    , "TAGame.CarComponent_Boost_TA:ReplicatedBoostAmount"
-    , "TAGame.CarComponent_TA:ReplicatedActive"
-    , "TAGame.GameEvent_Soccar_TA:ReplicatedScoredOnTeam"
-    , "TAGame.GameEvent_TA:ReplicatedStateIndex"
-    , "TAGame.PRI_TA:CameraPitch"
-    , "TAGame.PRI_TA:CameraYaw"
-    , "TAGame.Vehicle_TA:ReplicatedSteer"
-    , "TAGame.Vehicle_TA:ReplicatedThrottle"
-    ] & map StrictText.pack & Set.fromList
+byteProperties = Map.findWithDefault Set.empty "byte" properties
 
 
 -- | A set of properties that are camera settings.
+--
+-- >>> Set.member "TAGame.CameraSettingsActor_TA:ProfileSettings" camSettingsProperties
+-- True
 camSettingsProperties :: Set.Set StrictText.Text
-camSettingsProperties =
-    [ "TAGame.CameraSettingsActor_TA:ProfileSettings"
-    , "TAGame.PRI_TA:CameraSettings"
-    ] & map StrictText.pack & Set.fromList
+camSettingsProperties = Map.findWithDefault Set.empty "cam_settings" properties
 
 
 -- | A set of properties that are demolitions.
+--
+-- >>> Set.member "TAGame.Car_TA:ReplicatedDemolish" demolishProperties
+-- True
 demolishProperties :: Set.Set StrictText.Text
-demolishProperties =
-    [ "TAGame.Car_TA:ReplicatedDemolish"
-    ] & map StrictText.pack & Set.fromList
+demolishProperties = Map.findWithDefault Set.empty "demolish" properties
 
 
 -- | A set of properties that are enumerations.
+--
+-- >>> Set.member "Engine.Actor:Role" enumProperties
+-- True
 enumProperties :: Set.Set StrictText.Text
-enumProperties =
-    [ "Engine.Actor:Role"
-    ] & map StrictText.pack & Set.fromList
+enumProperties = Map.findWithDefault Set.empty "enum" properties
 
 
 -- | A set of properties that are explosions.
+--
+-- >>> Set.member "TAGame.Ball_TA:ReplicatedExplosionData" explosionProperties
+-- True
 explosionProperties :: Set.Set StrictText.Text
-explosionProperties =
-    [ "TAGame.Ball_TA:ReplicatedExplosionData"
-    ] & map StrictText.pack & Set.fromList
+explosionProperties = Map.findWithDefault Set.empty "explosion" properties
 
 
 -- | A set of properties that are flagged integers.
+--
+-- >>> Set.member "Engine.Actor:Owner" flaggedIntProperties
+-- True
 flaggedIntProperties :: Set.Set StrictText.Text
-flaggedIntProperties =
-    [ "Engine.Actor:Owner"
-    , "Engine.Actor:ReplicatedCollisionType"
-    , "Engine.GameReplicationInfo:GameClass"
-    , "Engine.Pawn:PlayerReplicationInfo"
-    , "Engine.PlayerReplicationInfo:Team"
-    , "Engine.TeamInfo:TeamIndex"
-    , "TAGame.Ball_TA:GameEvent"
-    , "TAGame.CameraSettingsActor_TA:PRI"
-    , "TAGame.CarComponent_TA:Vehicle"
-    , "TAGame.CrowdActor_TA:GameEvent"
-    , "TAGame.CrowdActor_TA:ReplicatedOneShotSound"
-    , "TAGame.CrowdManager_TA:GameEvent"
-    , "TAGame.CrowdManager_TA:ReplicatedGlobalOneShotSound"
-    , "TAGame.PRI_TA:PersistentCamera"
-    , "TAGame.PRI_TA:ReplicatedGameEvent"
-    , "TAGame.Team_TA:GameEvent"
-    , "TAGame.Team_TA:LogoData"
-    ] & map StrictText.pack & Set.fromList
+flaggedIntProperties = Map.findWithDefault Set.empty "flagged_int" properties
 
 
 -- | A set of properties that are floats.
+--
+-- >>> Set.member "Engine.Actor:DrawScale" floatProperties
+-- True
 floatProperties :: Set.Set StrictText.Text
-floatProperties =
-    [ "Engine.Actor:DrawScale"
-    , "TAGame.Ball_TA:ReplicatedAddedCarBounceScale"
-    , "TAGame.Ball_TA:ReplicatedBallMaxLinearSpeedScale"
-    , "TAGame.Ball_TA:ReplicatedBallScale"
-    , "TAGame.Ball_TA:ReplicatedWorldBounceScale"
-    , "TAGame.CarComponent_Boost_TA:BoostModifier"
-    , "TAGame.CarComponent_Boost_TA:RechargeDelay"
-    , "TAGame.CarComponent_Boost_TA:RechargeRate"
-    , "TAGame.CarComponent_FlipCar_TA:FlipCarTime"
-    , "TAGame.CrowdActor_TA:ModifiedNoise"
-    ] & map StrictText.pack & Set.fromList
+floatProperties = Map.findWithDefault Set.empty "float" properties
 
 
 -- | A set of properties that are game modes.
+--
+-- >>> Set.member "TAGame.GameEvent_TA:GameMode" gameModeProperties
+-- True
 gameModeProperties :: Set.Set StrictText.Text
-gameModeProperties =
-    [ "TAGame.GameEvent_TA:GameMode"
-    ] & map StrictText.pack & Set.fromList
+gameModeProperties = Map.findWithDefault Set.empty "game_mode" properties
 
 
 -- | A set of properties that are integers.
+--
+-- >>> Set.member "Engine.PlayerReplicationInfo:PlayerID" intProperties
+-- True
 intProperties :: Set.Set StrictText.Text
-intProperties =
-    [ "Engine.PlayerReplicationInfo:PlayerID"
-    , "Engine.PlayerReplicationInfo:Score"
-    , "Engine.TeamInfo:Score"
-    , "ProjectX.GRI_X:ReplicatedGameMutatorIndex"
-    , "ProjectX.GRI_X:ReplicatedGamePlaylist"
-    , "TAGame.CrowdActor_TA:ReplicatedCountDownNumber"
-    , "TAGame.GameEvent_Soccar_TA:RoundNum"
-    , "TAGame.GameEvent_Soccar_TA:SecondsRemaining"
-    , "TAGame.GameEvent_TA:BotSkill"
-    , "TAGame.GameEvent_TA:ReplicatedGameStateTimeRemaining"
-    , "TAGame.GameEvent_TA:ReplicatedStateName"
-    , "TAGame.GameEvent_Team_TA:MaxTeamSize"
-    , "TAGame.PRI_TA:MatchAssists"
-    , "TAGame.PRI_TA:MatchGoals"
-    , "TAGame.PRI_TA:MatchSaves"
-    , "TAGame.PRI_TA:MatchScore"
-    , "TAGame.PRI_TA:MatchShots"
-    , "TAGame.PRI_TA:Title"
-    , "TAGame.PRI_TA:TotalXP"
-    , "TAGame.Team_Soccar_TA:GameScore"
-    ] & map StrictText.pack & Set.fromList
+intProperties = Map.findWithDefault Set.empty "int" properties
 
 
 -- | A set of properties that are online loadouts.
+--
+-- >>> Set.member "TAGame.PRI_TA:ClientLoadoutOnline" loadoutOnlineProperties
+-- True
 loadoutOnlineProperties :: Set.Set StrictText.Text
-loadoutOnlineProperties =
-    [ "TAGame.PRI_TA:ClientLoadoutOnline"
-    ] & map StrictText.pack & Set.fromList
+loadoutOnlineProperties = Map.findWithDefault Set.empty "loadout_online" properties
 
 
 -- | A set of properties that are loadouts.
+--
+-- >>> Set.member "TAGame.PRI_TA:ClientLoadout" loadoutProperties
+-- True
 loadoutProperties :: Set.Set StrictText.Text
-loadoutProperties =
-    [ "TAGame.PRI_TA:ClientLoadout"
-    ] & map StrictText.pack & Set.fromList
+loadoutProperties = Map.findWithDefault Set.empty "loadout" properties
 
 
 -- | A set of properties that are locations.
+--
+-- >>> Set.member "Engine.Actor:RelativeLocation" locationProperties
+-- True
 locationProperties :: Set.Set StrictText.Text
-locationProperties =
-    [ "Engine.Actor:RelativeLocation"
-    , "TAGame.CarComponent_Dodge_TA:DodgeTorque"
-    ] & map StrictText.pack & Set.fromList
+locationProperties = Map.findWithDefault Set.empty "location" properties
 
 
 -- | A set of properties that are music stingers.
+--
+-- >>> Set.member "TAGame.GameEvent_Soccar_TA:ReplicatedMusicStinger" musicStingerProperties
+-- True
 musicStingerProperties :: Set.Set StrictText.Text
-musicStingerProperties =
-    [ "TAGame.GameEvent_Soccar_TA:ReplicatedMusicStinger"
-    ] & map StrictText.pack & Set.fromList
+musicStingerProperties = Map.findWithDefault Set.empty "music_stinger" properties
 
 
 -- | A set of properties that are pickups.
+--
+-- >>> Set.member "TAGame.VehiclePickup_TA:ReplicatedPickupData" pickupProperties
+-- True
 pickupProperties :: Set.Set StrictText.Text
-pickupProperties =
-    [ "TAGame.VehiclePickup_TA:ReplicatedPickupData"
-    ] & map StrictText.pack & Set.fromList
+pickupProperties = Map.findWithDefault Set.empty "pickup" properties
 
 
 -- | A set of properties that are private match settings.
+--
+-- >>> Set.member "TAGame.GameEvent_SoccarPrivate_TA:MatchSettings" privateMatchSettingsProperties
+-- True
 privateMatchSettingsProperties :: Set.Set StrictText.Text
-privateMatchSettingsProperties =
-    [ "TAGame.GameEvent_SoccarPrivate_TA:MatchSettings"
-    ] & map StrictText.pack & Set.fromList
+privateMatchSettingsProperties = Map.findWithDefault Set.empty "private_match_settings" properties
 
 
 -- | A set of properties that are qwords.
+--
+-- >>> Set.member "ProjectX.GRI_X:GameServerID" qWordProperties
+-- True
 qWordProperties :: Set.Set StrictText.Text
-qWordProperties =
-    [ "ProjectX.GRI_X:GameServerID"
-    ] & map StrictText.pack & Set.fromList
+qWordProperties = Map.findWithDefault Set.empty "qword" properties
 
 
 -- | A set of properties that are relation rotations.
+--
+-- >>> Set.member "Engine.Actor:RelativeRotation" relativeRotationProperties
+-- True
 relativeRotationProperties :: Set.Set StrictText.Text
-relativeRotationProperties =
-    [ "Engine.Actor:RelativeRotation"
-    ] & map StrictText.pack & Set.fromList
+relativeRotationProperties = Map.findWithDefault Set.empty "relative_rotation" properties
 
 
 -- | A set of properties that are reservations.
+--
+-- >>> Set.member "ProjectX.GRI_X:Reservations" reservationProperties
+-- True
 reservationProperties :: Set.Set StrictText.Text
-reservationProperties =
-    [ "ProjectX.GRI_X:Reservations"
-    ] & map StrictText.pack & Set.fromList
+reservationProperties = Map.findWithDefault Set.empty "reservation" properties
 
 
 -- | A set of properties that are rigid body states.
+--
+-- >>> Set.member "TAGame.RBActor_TA:ReplicatedRBState" rigidBodyStateProperties
+-- True
 rigidBodyStateProperties :: Set.Set StrictText.Text
-rigidBodyStateProperties =
-    [ "TAGame.RBActor_TA:ReplicatedRBState"
-    ] & map StrictText.pack & Set.fromList
+rigidBodyStateProperties = Map.findWithDefault Set.empty "rigid_body_state" properties
 
 
 -- | A set of properties that are strings.
+--
+-- >>> Set.member "Engine.GameReplicationInfo:ServerName" stringProperties
+-- True
 stringProperties :: Set.Set StrictText.Text
-stringProperties =
-    [ "Engine.GameReplicationInfo:ServerName"
-    , "Engine.PlayerReplicationInfo:PlayerName"
-    , "Engine.PlayerReplicationInfo:RemoteUserData"
-    , "TAGame.GRI_TA:NewDedicatedServerIP"
-    , "TAGame.Team_TA:CustomTeamName"
-    ] & map StrictText.pack & Set.fromList
+stringProperties = Map.findWithDefault Set.empty "string" properties
 
 
 -- | A set of properties that are team paints.
+--
+-- >>> Set.member "TAGame.Car_TA:TeamPaint" teamPaintProperties
+-- True
 teamPaintProperties :: Set.Set StrictText.Text
-teamPaintProperties =
-    [ "TAGame.Car_TA:TeamPaint"
-    ] & map StrictText.pack & Set.fromList
+teamPaintProperties = Map.findWithDefault Set.empty "team_paint" properties
 
 
 -- | A set of properties that are unique IDs.
+--
+-- >>> Set.member "Engine.PlayerReplicationInfo:UniqueId" uniqueIdProperties
+-- True
 uniqueIdProperties :: Set.Set StrictText.Text
-uniqueIdProperties =
-    [ "Engine.PlayerReplicationInfo:UniqueId"
-    -- , "TAGame.PRI_TA:PartyLeader"
-    ] & map StrictText.pack & Set.fromList
+uniqueIdProperties = Map.findWithDefault Set.empty "unique_id" properties
+
+
+properties :: Map.Map StrictText.Text (Set.Set StrictText.Text)
+properties = $(FileEmbed.embedFile "data/properties.json")
+    & Aeson.decodeStrict
+    & Maybe.fromMaybe Map.empty
