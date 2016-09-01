268a9731d13e2c980102516e6f5cb593bcd67e3c
Taylor Fausak <taylor@fausak.me>
2016-09-01 10:34:59 -0500

    Move playstation IDs into their own module

--- library/Octane/Type/RemoteId.hs
+++ library/Octane/Type/RemoteId.hs
@@ -1,82 +1,25 @@
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StrictData #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeFamilies #-}
 
 module Octane.Type.RemoteId
   ( RemoteId(..)
+  , module Octane.Type.RemoteId.PlayStationId
   , module Octane.Type.RemoteId.SplitscreenId
   , module Octane.Type.RemoteId.SteamId
-  , PlayStationId(..)
   , module Octane.Type.RemoteId.XboxId
   ) where
 
 import Data.Aeson ((.=))
-import Data.Function ((&))
+import Octane.Type.RemoteId.PlayStationId
 import Octane.Type.RemoteId.SplitscreenId
 import Octane.Type.RemoteId.SteamId
 import Octane.Type.RemoteId.XboxId
 
 import qualified Control.DeepSeq as DeepSeq
 import qualified Data.Aeson as Aeson
-import qualified Data.Binary.Bits as BinaryBit
-import qualified Data.Binary.Bits.Get as BinaryBit
-import qualified Data.Binary.Bits.Put as BinaryBit
-import qualified Data.ByteString.Lazy as LazyBytes
-import qualified Data.Default.Class as Default
-import qualified Data.OverloadedRecords.TH as OverloadedRecords
-import qualified Data.Text as StrictText
-import qualified Data.Text.Encoding as Encoding
 import qualified GHC.Generics as Generics
 import qualified Octane.Type.Text as Text
-import qualified Octane.Utility.Endian as Endian
-import qualified Text.Printf as Printf
-
-data PlayStationId = PlayStationId
-  { playStationIdName :: Text.Text
-  , playStationIdUnknown :: LazyBytes.ByteString
-  } deriving (Eq, Generics.Generic, Show)
-
--- | Each part is stored as exactly 16 bits.
-instance BinaryBit.BinaryBit PlayStationId where
-  getBits _ = do
-    nameBytes <- BinaryBit.getByteString 16
-    let name =
-          nameBytes & Endian.reverseBitsInStrictBytes & Encoding.decodeLatin1 &
-          StrictText.dropWhileEnd (== '\0') &
-          Text.Text
-    unknownBytes <- BinaryBit.getByteString 16
-    let unknown =
-          unknownBytes & Endian.reverseBitsInStrictBytes & LazyBytes.fromStrict
-    pure (PlayStationId name unknown)
-  putBits _ playStationId = do
-    playStationId & #name & #unpack & StrictText.justifyLeft 16 '\x00' &
-      StrictText.take 16 &
-      Text.encodeLatin1 &
-      Endian.reverseBitsInStrictBytes &
-      BinaryBit.putByteString
-    playStationId & #unknown & LazyBytes.toStrict &
-      Endian.reverseBitsInStrictBytes &
-      BinaryBit.putByteString
-
-instance DeepSeq.NFData PlayStationId
-
-instance Aeson.ToJSON PlayStationId where
-  toJSON playStationId =
-    Aeson.object
-      [ "Name" .= #name playStationId
-      , "Unknown" .=
-        (playStationId & #unknown & LazyBytes.unpack &
-         concatMap (Printf.printf "%02x") &
-         ("0x" ++) &
-         StrictText.pack)
-      ]
 
 -- | A player's canonical remote ID. This is the best way to uniquely identify
 -- players
@@ -86,8 +29,6 @@ data RemoteId
   | RemoteSteamId SteamId
   | RemoteXboxId XboxId
   deriving (Eq, Generics.Generic, Show)
-
-$(OverloadedRecords.overloadedRecords Default.def [''PlayStationId])
 
 instance DeepSeq.NFData RemoteId
 
