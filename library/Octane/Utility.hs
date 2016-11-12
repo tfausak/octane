e7734d196427fd28c4283558f9388d16f6308b6f
Taylor Fausak <taylor@fausak.me>
2016-11-12 10:36:59 -0600

    Remove generator

--- library/Octane/Utility.hs
+++ library/Octane/Utility.hs
@@ -1,9 +1,7 @@
 module Octane.Utility
   ( module Octane.Utility.Embed
   , module Octane.Utility.Endian
-  , module Octane.Utility.Generator
   ) where
 
 import Octane.Utility.Embed
 import Octane.Utility.Endian
-import Octane.Utility.Generator
